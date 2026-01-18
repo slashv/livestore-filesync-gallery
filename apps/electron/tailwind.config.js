52288930ec32ac8cfccbdb4077082f3fe887a96d
Victor Nyberg <contact@victornyberg.com>
2026-01-18 18:23:00 +0100

    fix shared ui styling

--- apps/electron/tailwind.config.js
+++ apps/electron/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/renderer/**/*.{js,ts,jsx,tsx,html}'],
+  content: [
+    './src/renderer/**/*.{js,ts,jsx,tsx,html}',
+    '../../packages/ui/src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
